e79f9e53ffe8760fc0f7051baf7a618ecdc46f1a
Badal Moradia <badal@i360llc.com>
2018-07-23 12:09:47 -0500

    added same password check to create new user

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Javascript is linked');
+
+$('#newUserButton').prop('disabled', true)
+$('#newUserButton').css('cursor', 'not-allowed')
+
+
+
+//add event listener for when use clicks out of the input form
+//checks if both password fields are the same in order to enable submit button
+$('input').on('keyup', (event) => {
+	$password1 = $('#pass');
+	$password2 = $('#checkpass');
+	// console.log(typeof $password1.val());
+	// console.log($password1.val() == '')
+	// //console.log($password2.val());	
+
+	if(($password1.val() !== $password2.val()) || $password1.val() == '' || $password2.val() == '') {
+		console.log('got in here')
+		$('#newUserButton').prop('disabled', true)
+		$('#newUserButton').css('cursor', 'not-allowed')
+	} else {
+		$('#newUserButton').prop('disabled', false)
+		$('#newUserButton').css('cursor', 'pointer')
+	}
+
+})
+
